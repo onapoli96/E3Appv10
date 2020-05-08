c06a9481f874bb7860c3173025dd0b5cd1e37709
Giovanni <pasqualemagno@gmail.com>
2020-05-08 10:21:01 +0200

    Lettura dei messaggi sottoforma di oggetti json

--- app/src/main/java/JavaClasses/Messaggio.java
+++ app/src/main/java/JavaClasses/Messaggio.java
@@ -1,12 +1,37 @@
 package JavaClasses;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Messaggio {
     private String messaggio;
     private String orario;
+    private String nome;
+    private String cognome;
 
-    public Messaggio(String messaggio, String orario){
-        this.messaggio = messaggio;
+    public Messaggio(String messaggio, String orario) throws JSONException {
+        System.out.println("JSON top: "+messaggio);
+        JSONObject json = new JSONObject(messaggio);
+        this.nome = json.getString("nome");
+        this.cognome = json.getString("cognome");
+        this.messaggio = json.getString("msg");
         this.orario = orario;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     public String getMessaggio() {
