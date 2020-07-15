6e99a50b8ea40e867b57ca8854497ad10ad97cb9
Giovanni <pasqualemagno@gmail.com>
2020-07-15 20:02:17 +0200

    modifiche grafiche 2

--- app/src/main/java/com/example/e3appv10/giorgio/customs/CustomView.java
+++ app/src/main/java/com/example/e3appv10/giorgio/customs/CustomView.java
@@ -30,8 +30,8 @@ public class CustomView extends View {
         //int x = (int)((nodo.getX() - 8 )* density);
         //int y = (int)((nodo.getY() - 64)* density);
         //int y = (int)((nodo.getY()-150)* density/1.5);
-        int x = (int)((nodo.getX() - 8 )* density/1.5);
-        int y = (int)((nodo.getY()-68)* density);
+        int x = (int)((nodo.getX() - 8 )* density);
+        int y = (int)((nodo.getY()-64)* density);
         canvas.drawCircle(x, y, 20, esterno);
         canvas.drawCircle(x, y, 15, interno);
         Paint paint = new Paint();
